8cfd4322ccef1cd30d4532a0670dc3e6647bd064
zhiyeTan <594557148@qq.com>
2019-12-10 22:34:53 +0800

    优化

--- resource/app/4cm/component/builder/script.js
+++ resource/app/4cm/component/builder/script.js
@@ -3,59 +3,30 @@ const Builder = {
 	loginApi: '/4cm/login',
 	init: function(){
 		Backdrop.init();
-		//this.initLoginForm();
-		this.initStruct();
+		Dialog.init();
+		this.initLoginForm();
+		//this.initStruct();
 	},
 	/**
 	 * 初始化登录表单
 	 */
 	initLoginForm: function(){
-		let loginBox = document.createElement('div');
-		loginBox.id = 'loginBox';
-		loginBox.className = 'container';
-		loginBox.style.marginTop = (document.documentElement.clientHeight / 2 - 150) + 'px';
-		let formObj = document.createElement('form');
-		formObj.className = 'col-xs-8 col-xs-offset-2';
-		//应用名
-		let appTitleObj = document.createElement('div');
-		appTitleObj.className = 'form-group text-center h2';
-		appTitleObj.innerHTML = this.appName;
-		formObj.append(appTitleObj);
-		//账号
-		let accountObj = document.createElement('input');
-		accountObj.className = 'form-control';
-		accountObj.type = 'text';
-		accountObj.name = 'account';
-		accountObj.placeholder = '账号';
-		let accountBox = document.createElement('div');
-		accountBox.className = 'form-group';
-		accountBox.append(accountObj);
-		formObj.append(accountBox);
-		//密码
-		let passwordObj = document.createElement('input');
-		passwordObj.className = 'form-control';
-		passwordObj.type = 'password';
-		passwordObj.name = 'password';
-		passwordObj.placeholder = '密码';
-		let passwordBox = document.createElement('div');
-		passwordBox.className = 'form-group';
-		passwordBox.append(passwordObj);
-		formObj.append(passwordBox);
-		//信息提示框
-		let tipsObj = document.createElement('div');
-		tipsObj.className = 'alert alert-danger d-none';
-		formObj.append(tipsObj);
-		//按钮
-		let btnObj = document.createElement('button');
-		btnObj.type = 'submit';
-		btnObj.className = 'btn btn-block btn-success';
-		btnObj.innerHTML = '登录';
-		let btnBox = document.createElement('div');
-		btnBox.className = 'form-group';
-		btnBox.append(btnObj);
-		formObj.append(btnBox);
-		loginBox.append(formObj);
-		$('body').append(loginBox)
+		$('body').append('<div id="loginBox" class="container">' +
+							'<form class="col-xs-8 col-xs-offset-2">' +
+								'<div class="form-group text-center h2">4CM</div>' +
+								'<div class="form-group">' +
+									'<input class="form-control" type="text" name="account" placeholder="账号">' +
+								'</div>' +
+								'<div class="form-group">' +
+									'<input class="form-control" type="password" name="password" placeholder="密码">' +
+								'</div>' +
+								'<div class="alert alert-danger d-none"></div>' +
+								'<div class="form-group">' +
+									'<button type="submit" class="btn btn-block btn-success">登录</button>' +
+								'</div>' +
+							'</form>' +
+						'</div>');
+		$('#loginBox').css('margin-top', (document.documentElement.clientHeight / 2 - 150) + 'px');
 		//绑定异步提交登录
 		let self = this;
 		$('#loginBox').on('submit', function(){
--- resource/app/4cm/component/dialog/script.js
+++ resource/app/4cm/component/dialog/script.js
@@ -1,32 +1,60 @@
 const Dialog = {
+	init: function(){
+		$('body').append('<div id="dialog" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="fcmDialogTitle" aria-hidden="true">' +
+							'<div class="modal-dialog">' +
+								'<div class="modal-content">' +
+									'<div class="modal-header">' +
+										'<span class="modal-title"></span>' +
+										'<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+									'</div>' +
+									'<div class="modal-body"></div>' +
+									'<div class="modal-footer">' +
+										'<button name="cancel" type="button" class="btn btn-default" data-dismiss="modal">取消</button>' +
+										'<button name="confirm" type="button" class="btn btn-primary">确定</button>' +
+									'</div>' +
+								'</div>' +
+							'</div>' +
+						'</div>');
+	},
 	/**
+	 * 显示对话框
 	 * @param {Object} args 样式参数
 	 * {
 	 *     title: 对话框标题
 	 *     message: 对话框提示信息
 	 *     btnyes: 是否显示确认按钮
 	 *     btnno: 是否显示取消按钮
+	 *     confirm: 确认按钮的回调函数
+	 *     cancel: 取消按钮的回调函数
 	 * }
 	 */
-	set: function(args){
-		$('#fcmDialogTitle').html(args.title ? args.title : '系统提示');
-		$('#fcmDialogContent').html(args.message ? args.message : '暂无明细信息');
+	show: function(args){
+		$('#dialog .modal-title').html(args.title ? args.title : '系统提示');
+		$('#dialog .modal-body').html(args.message ? args.message : '暂无明细信息');
 		if(args.hasOwnProperty('btnyes') && !args.btnyes){
-			$('#fcmBtnYes').hide();
+			$('#dialog [name=confirm]').hide();
 		}
 		else{
-			$('#fcmBtnYes').show();
+			$('#dialog [name=confirm]').show();
 		}
 		if(args.hasOwnProperty('btnno') && !args.btnno){
-			$('#fcmBtnNo').hide();
+			$('#dialog [name=cancel]').hide();
 		}
 		else{
-			$('#fcmBtnNo').show();
+			$('#dialog [name=cancel]').show();
 		}
 		//绑定默认的点击事件
-		$('#fcmBtnYes,#fcmBtnNo').off('click').on('click', function(){
-			$('#fcmDialog').modal('hide');
+		$('#dialog [name=confirm],#dialog [name=cancel]').off('click').on('click', function(){
+			$('#dialog').modal('hide');
 		});
+		if(args.confirm){
+			this.confirm(args.confirm);
+		}
+		if(args.cancel){
+			this.cancel(args.cancel);
+		}
+		$('#dialog .modal-dialog').css('top', '18%');
+		$('#dialog').modal('show');
 		return this;
 	},
 	/**
@@ -34,9 +62,9 @@ const Dialog = {
 	 * @param {Object} callBack
 	 */
 	confirm: function(callBack){
-		$('#fcmBtnYes').off('click').on('click', function(){
+		$('#dialog [name=confirm]').off('click').on('click', function(){
 			callBack();
-			$('#fcmDialog').modal('hide');
+			$('#dialog').modal('hide');
 		});
 		return this;
 	},
@@ -45,17 +73,10 @@ const Dialog = {
 	 * @param {Object} callBack
 	 */
 	cancel: function(callBack){
-		$('#fcmBtnNo').off('click').on('click', function(){
+		$('#dialog [name=cancel]').off('click').on('click', function(){
 			callBack();
-			$('#fcmDialog').modal('hide');
+			$('#dialog').modal('hide');
 		});
 		return this;
 	},
-	/**
-	 * 显示对话框
-	 */
-	show: function(){
-		$('#fcmDialog .modal-dialog').css('top', '18%');
-		$('#fcmDialog').modal('show');
-	}
 }
